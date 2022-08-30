66c757b154c2b268aa76656704e15849ae0f9665
sriparan <19173503+sriparan@users.noreply.github.com>
2022-08-29 22:27:42 -0700

    Adding sts call

--- srctop/simple-webapp/echo-express/standalone/src/http-server.ts
+++ srctop/simple-webapp/echo-express/standalone/src/http-server.ts
@@ -3,6 +3,8 @@ import http from "http";
 import https from "https";
 import dotenv from "dotenv";
 import fs from "fs";
+import STS from "aws-sdk/clients/sts";
+import { AWSError } from "aws-sdk";
 
 dotenv.config();
 
@@ -15,12 +17,24 @@ if (typeof process.env.PORT == "string") {
 }
 
 app.get("/*", (req, res) => {
-  const dataobj: object = {
+  const dataobj = {
     path: req.url,
     params: req.params,
     headers: req.headers,
     originalURL: req.originalUrl,
+    callerId: {},
   };
+  const mysts = new STS();
+  const callerIdentity = mysts.getCallerIdentity(
+    {},
+    (err: AWSError, data: STS.Types.GetCallerIdentityResponse) => {
+      if (err) {
+        console.log(err);
+      } else {
+        dataobj.callerId = data;
+      }
+    }
+  );
   const resp: string = JSON.stringify(dataobj);
   console.log(`received ${count} ${resp}`);
   res.send(`REquest count  ${count} - INPUT-  ${resp}`);
